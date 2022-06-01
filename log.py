88ca397b3b6679ba6b8bc678b116a8ccfd5dcc57
Vedant <vsagar@wisc.edu>
2022-05-31 22:24:24 -0500

    Tuesday

--- log.py
+++ log.py
@@ -18,7 +18,7 @@ ax.set_xlabel('Date')
 ax.set_ylabel('Calories consumed')
 
 plt.plot(data.keys(), data.values(), color='#2B709F', marker="o")
-plt.axhline(y=2000, color="#566573", ls="--")
+plt.axhline(y=2000, color="#C23737", ls="--")
 plt.yticks([0, 500, 1000, 1500, 2000, 2500, 3000])
 
 ax.spines['right'].set_visible(False)
